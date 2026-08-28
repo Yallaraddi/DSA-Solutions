00fcc09917ec0624a8401ec8234cdababb0e0c8a
Yallaraddi sidnal <sidnalyallaraddi@gmail.com>
2026-08-28 17:33:49 +0530

    Optimize singleNumber function implementation
    Refactor singleNumber function to eliminate unnecessary vectors and simplify logic.

--- 260-single-number-iii/single-number-iii.cpp
+++ 260-single-number-iii/single-number-iii.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        vector<int> arr1;
-        vector<int> arr2;
+        int ans1 = 0, ans2 = 0;
         int n = nums.size();
         int ans = 0;
         for (int i = 0; i < n; i++) {
@@ -18,17 +17,10 @@ public:
         }
         for (int i = 0; i < n; i++) {
             if (nums[i] & (1 << bit_p)) {
-                arr1.push_back(nums[i]);
+                ans1 = ans1 ^ nums[i];
             } else {
-                arr2.push_back(nums[i]);
+                ans2 = ans2 ^ nums[i];
             }
-        }
-        int ans1 = 0, ans2 = 0;
-        for (auto it : arr1) {
-            ans1 = ans1 ^ it;
-        }
-        for (auto it : arr2) {
-            ans2 = ans2 ^ it;
         }
         return {ans1, ans2};
     }

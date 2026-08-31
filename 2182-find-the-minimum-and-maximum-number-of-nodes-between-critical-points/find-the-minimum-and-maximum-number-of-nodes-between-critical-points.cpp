8a931f2dd62cc9527dfd7ab9e0d60a19a0dd4a51
Yallaraddi sidnal <sidnalyallaraddi@gmail.com>
2026-08-31 10:37:06 +0530

    Time: 0 ms (100.00%) | Memory: 124.7 MB (60.57%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -11,36 +11,32 @@
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
-        ListNode* curr=head->next;
-        ListNode* previ=head;
-        ListNode* front=head->next->next;
+        ListNode* curr = head->next;
+        ListNode* previ = head;
+        ListNode* front = head->next->next;
         vector<int> ans;
-        int idx=2;
-        int maxd=INT_MIN,mind=INT_MAX;
-        vector<int> arr;
-        while(curr->next!=NULL){
-            if(curr->val > previ->val && curr->val > front->val){
-                arr.push_back(idx);
-            }else if(curr->val < previ->val && curr->val < front->val){
-                arr.push_back(idx);
+        int idx = 2;
+        int first = 0, last = 0, mindist = INT_MAX, prev_idx = 0;
+
+        while (curr->next != NULL) {
+            if ((curr->val > previ->val && curr->val > front->val) ||
+                (curr->val < previ->val && curr->val < front->val)) {
+                if (first == 0) {
+                    first = idx;
+                } else {
+                    mindist = min(mindist, idx - prev_idx);
+                }
+                last = idx;
+                prev_idx = idx;
             }
-            previ=curr;
-            curr=front;
-            front=front->next;
+            previ = curr;
+            curr = front;
+            front = front->next;
             idx++;
         }
-        int n=arr.size();
-        if(n==0 || n==1){
-            return {-1,-1};
+        if (first == 0 || last == 0 || first==last) {
+            return {-1, -1};
         }
-        maxd=arr[n-1]-arr[0];
-        for(int i=1;i<n;i++){
-            int dis=arr[i]-arr[i-1];
-            mind=min(mind,dis);
-            if(mind==1){
-                break;
-            }
-        }
-        return {mind,maxd};
+        return {mindist, last - first};
     }
 };

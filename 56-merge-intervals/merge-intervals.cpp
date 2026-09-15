4f2b94ac5d44b6a60c59dfd688bd319f3659cf2a
Yallaraddi sidnal <sidnalyallaraddi@gmail.com>
2026-09-15 20:54:46 +0530

    Time: 7 ms (39.64%) | Memory: 23.9 MB (60.35%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& nums) {
+        int n=nums.size();
+        vector<vector<int>> ans;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n;i++){
+            if(ans.empty() || nums[i][0]>ans.back()[1]){
+                ans.push_back(nums[i]);
+            }else{
+                ans.back()[1]=max(ans.back()[1],nums[i][1]);
+            }
+        }
+        return ans;
+        
+    }
+};

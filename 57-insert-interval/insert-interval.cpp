149bbbcd7e3da13d3a8765cfd6db67f26b603359
Yallaraddi sidnal <sidnalyallaraddi@gmail.com>
2026-09-16 11:04:57 +0530

    Time: 0 ms (100.00%) | Memory: 21.7 MB (76.19%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,5 +1,14 @@
 class Solution {
 public:
+    void helper(vector<vector<int>>& ans, vector<int>& newInt) {
+        if (ans.empty() || ans.back()[1] < newInt[0]) {
+            ans.push_back({newInt[0], newInt[1]});
+
+        } else {
+            ans.back()[1] = max(ans.back()[1], newInt[1]);
+            ans.back()[0] = min(ans.back()[0], newInt[0]);
+        }
+    }
     vector<vector<int>> insert(vector<vector<int>>& nums, vector<int>& newInt) {
         vector<vector<int>> ans;
         int n = nums.size();
@@ -7,19 +16,12 @@ public:
             ans.push_back(newInt);
             return ans;
         }
-        // ans.push_back(nums[0]);
         int flag = 0;
         for (int i = 0; i < n; i++) {
             if (flag == 0 && nums[i][0] > newInt[0]) {
                 flag = 1;
 
-                if (ans.empty() || ans.back()[1] < newInt[0]) {
-                    ans.push_back({newInt[0], newInt[1]});
-
-                } else {
-                    ans.back()[1] = max(ans.back()[1], newInt[1]);
-                    ans.back()[0] = min(ans.back()[0], newInt[0]);
-                }
+                helper(ans,newInt);
             }
 
             if (ans.empty() || ans.back()[1] < nums[i][0]) {
@@ -29,13 +31,7 @@ public:
             }
         }
         if (flag == 0) {
-            if (ans.empty() || ans.back()[1] < newInt[0]) {
-                ans.push_back({newInt[0], newInt[1]});
-
-            } else {
-                ans.back()[1] = max(ans.back()[1], newInt[1]);
-                ans.back()[0] = min(ans.back()[0], newInt[0]);
-            }
+            helper(ans,newInt);
         }
         return ans;
     }

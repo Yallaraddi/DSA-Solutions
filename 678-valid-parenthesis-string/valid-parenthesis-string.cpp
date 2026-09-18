5dad3c67183ab190635d337ccf99dc8eaf2c9560
Yallaraddi sidnal <sidnalyallaraddi@gmail.com>
2026-09-18 12:41:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (74.46%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -2,35 +2,21 @@ class Solution {
 public:
     bool checkValidString(string s) {
         int n = s.length();
-        stack<int> st1;
-        stack<int> st2;
-        int cnt = 0;
-        for (int i = 0; i < n; i++) {
-            if (s[i] == '(') {
-                st1.push(i);
-            } else if (s[i] == '*') {
-                st2.push(i);
-            } else {
-                if (st1.empty() && st2.empty()) {
-                    return false;
-                }
-                if(!st1.empty()){
-                    st1.pop();
-                }else{
-                    st2.pop();
-                }
-
+        int minOpen=0,maxOpen=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='('){
+                minOpen++;
+                maxOpen++;
+            }else if(s[i]==')'){
+                minOpen--;
+                maxOpen--;
+            }else{
+                minOpen--;
+                maxOpen++;
             }
+            if(minOpen<0) minOpen=0;
+            if(maxOpen<0) return false;
         }
-        //if(st1.empty()) return true;
-
-        while(!st1.empty() && !st2.empty()){
-            if(st1.top()>st2.top()){
-                return false;
-            }
-            st1.pop();
-            st2.pop();
-        }
-        return st1.empty();
+        return minOpen==0;
     }
 };
